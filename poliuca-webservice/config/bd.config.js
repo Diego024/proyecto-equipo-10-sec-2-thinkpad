07ac631684e733d178262f94fc8de7752fca493e
Mauricio-mty <00000422@uca.edu.sv>
2023-11-20 20:39:59 -0600

    feat: Conexion a BD :wrench::sparkles:

---
+++ poliuca-webservice/config/bd.config.js
@@ -0,0 +1,41 @@
+//Importando mongoose de
+const Mongoose = require("mongoose");
+//Importando Debug 
+const debug = require("debug")("app.database");
+
+//Estos elementos se obtienen de variables de entorno
+const dbhost=process.env.DBHOST || "localhost";
+const dbport=process.env.DBPORT || "27017";
+const dbname=process.env.DBNAME || "poliuca-bd";
+
+//Uri de conexion:
+const dburi =process.env.DBURI ||`mongodb://${dbhost}:${dbport}/${dbname}`;
+
+
+//Metodo Conectar
+const connect = async () =>{
+    try{
+       await Mongoose.connect(dburi);
+       debug("Conexion realizada");
+    }catch(error){
+       console.error(error);
+       debug("No se pudo conectar a BD");
+       process.exit(1);
+    }
+}
+
+//Metodo Desconectar*/
+const disconnect = async()=>{
+    try {
+        await Mongoose.disconnect();
+        debug("Conexion finalizada");
+    } catch (error) {
+        process.exit(1);
+    }
+}
+
+//Exportacion de metodos
+module.exports={
+    connect,
+    disconnect
+};
