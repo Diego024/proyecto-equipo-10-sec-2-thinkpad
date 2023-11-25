45e9353cd4eadb3e60912163f1cddc2c4d3ddd38
diego.rosa <diego.rosa@mag.gob.sv>
2023-11-25 12:17:53 -0600

    :lipstick: Agregar interfaz de LogIn

--- poliuca-client/src/App.jsx
+++ poliuca-client/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "./app.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Calendario from "./assets/components/views/Calendario";
+import LogIn from "./assets/components/views/LogIn";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>¡Bienvenido!</h1>} />
+          <Route path="/" element={<LogIn />} />
           <Route path="/calendario" element={<Calendario />} />
         </Routes>
       </BrowserRouter>
