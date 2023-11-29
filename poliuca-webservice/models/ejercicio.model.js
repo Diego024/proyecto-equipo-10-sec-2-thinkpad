88a01a34014c17c06f3d2a5d0edd83795b5ae761
Mauricio-mty <00000422@uca.edu.sv>
2023-11-29 11:13:37 -0600

    ejericio controller fixing

--- poliuca-webservice/models/ejercicio.model.js
+++ poliuca-webservice/models/ejercicio.model.js
@@ -2,10 +2,29 @@ const Moongose = require("mongoose");
 const Scheme = Moongose.schema;
 
 const EjercicioSchema = new Scheme({
-name:{},
-grupo_muscular:{},
-imagen:{},
-equipo:{}
+name:{
+    type:String,
+    trim:true,
+    required:true,
+},
+grupo_muscular:{
+    type:String,
+    trim:true,
+    required:true,
+},
+imagen:{
+    type:String,
+    trim:true,
+    required:true,},
+equipo:{
+    type:String,
+    trim:true,
+    required:true,
+},
+hidden:{
+    type:Boolean,
+    default:false
+}
 },{timiestamps:true});
 
 module.export = Moongose.model("Ejericio",EjercicioSchema);
