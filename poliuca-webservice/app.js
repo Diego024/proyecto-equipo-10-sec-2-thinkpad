3a81908a84910491ed45dc7a6e9e3a4a2d96edfe
Mauricio-mty <104952595+Mauricio-mty@users.noreply.github.com>
2023-12-01 21:03:53 -0600

    Update app.js

--- poliuca-webservice/app.js
+++ poliuca-webservice/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 databse.connect();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,8 +19,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api",apiRouter);
-const port =3500;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
 module.exports = app;
